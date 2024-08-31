26087326cdc136e55d24903a185944ee64fde8f5
zahra davardoust <za.davardoust@gmail.com>
2024-08-31 14:59:29 +0330

    ui changes

--- lib/widgets/NewTransaction.dart
+++ lib/widgets/NewTransaction.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewTranaction extends StatefulWidget {
   final Function addTx;
@@ -13,18 +14,44 @@ class NewTranaction extends StatefulWidget {
 class _NewTransactionState extends State<NewTranaction> {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+  DateTime _selectedDate = DateTime.now() ;
 
 
-  void submitData (){
-    final enterTitle = titleController.text;
-    final enterAmount = double.parse(amountController.text);
-
-    if (enterTitle.isEmpty || enterAmount<0){
+  void submitData() {
+    if (amountController.text.isEmpty) {
       return;
     }
-    widget.addTx(enterTitle, enterAmount);
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
+      return;
+    }
+
+    widget.addTx(
+      enteredTitle,
+      enteredAmount,
+      _selectedDate,
+    );
 
     Navigator.of(context).pop();
+  }
+
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+    print('...');
   }
 
   @override
@@ -48,21 +75,47 @@ class _NewTransactionState extends State<NewTranaction> {
                 keyboardType: TextInputType.number,
                 onSubmitted: (_) =>  submitData,
               ),
-              TextButton(
-                onPressed: submitData,
-                child: Container(
-                  color: Colors.green,
-                  padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                  child: const Text(
-                    'Flat Button',
-                    style: TextStyle(color: Colors.white, fontSize: 13.0),
-                  ),
+              Container(
+                height: 70,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        _selectedDate == null
+                            ? 'No Date Chosen!'
+                            : 'Picked Date:     ${DateFormat.yMd().format(_selectedDate)}',
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: _presentDatePicker,
+                      child: Container(
+                        color: Colors.cyanAccent,
+                        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                        child: const Text(
+                          'Flat Button',
+                          style: TextStyle(color: Colors.white, fontSize: 13.0),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
+               TextButton(
+                  onPressed: submitData,
+                 child: Container(
+                   color: Colors.pinkAccent,
+                   padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                   child: const Text('Add Transaction',
+                     style: TextStyle(color: Colors.white, fontSize: 13.0),
+                   ),
+                 ),
+               ),
+
             ],
           ),
         ),
       );
   }
-  
+
 }
