c81e0a4de82d79af4fc956b2e5c7cd2a41d6db37
zahra davardoust <za.davardoust@gmail.com>
2024-08-31 07:16:03 +0330

    bottomsheet added to priject

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:personal_expence_app/models/Transaction.dart';
 import 'package:personal_expence_app/widgets/NewTransaction.dart';
+import 'package:personal_expence_app/widgets/chart.dart';
+import 'package:personal_expence_app/widgets/transaction_list.dart';
 import 'package:personal_expence_app/widgets/user_transaction.dart';
 
 void main() {
@@ -37,44 +39,89 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  int _counter = 0;
+  final List<Transaction> _userTransactions = [
+    // Transaction(
+    //   id: 't1',
+    //   title: 'New Shoes',
+    //   amount: 69.99,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't2',
+    //   title: 'Weekly Groceries',
+    //   amount: 16.53,
+    //   date: DateTime.now(),
+    // ),
+  ];
 
-  void _incrementCounter() {
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+
     setState(() {
-      _counter++;
+      _userTransactions.add(newTx);
     });
   }
+
+
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTranaction(_addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(
+          'Personal Expenses',
+        ),
         actions: <Widget>[
-          IconButton(onPressed: () {},
-              icon: Icon(Icons.add))
-        ],
-      ),
-      body: Column(
-        children: <Widget>[
-          Card(
-            child: Container (
-                child: Text('Chart'),
-                width: double.maxFinite,
-                margin: const EdgeInsets.all( 10.0),
-            ),
-            elevation: 5,
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _startAddNewTransaction(context),
           ),
-          UserTransaction()
         ],
       ),
-
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Chart(_recentTransactions),
+            TranslationList(_userTransactions),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
+      ),
     );
   }
 }
